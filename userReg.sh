27ff4cc42843fe721c456d2099b9b04bd6398339
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2020-08-11 03:00:18 +0530

    modified file in usecase2

--- userReg.sh
+++ userReg.sh
@@ -2,13 +2,21 @@
 #echo "this is master class"
 
 read -p "Enter the first name = " firstname
-
 pattern="^([[:upper:]]{1})([[:lower:]]{2,})$"
-
 if [[ $firstname =~ $pattern ]]
 then
         echo $firstname;
 else
 	echo "The first letter should be in capital"
+        exit
+fi
+
+read -p "Enter the last name = " lastname
+if [[ $lastname =~ $pattern ]]
+then
+        echo $lastname;
+else
+        echo "The first letter should be in capital"
+	exit
 fi
 
